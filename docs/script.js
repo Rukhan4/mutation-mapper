2037733bbef43545740371ef8a5482d272856d59
Rukhan4 <surur.rkhan@gmail.com>
2025-06-21 17:07:16 -0400

    docs for github pages

--- docs/script.js
+++ docs/script.js
@@ -12,7 +12,7 @@ function upload() {
   const formData = new FormData();
   formData.append("file", file);
 
-  fetch("https://mutation-mapper-backend.onrender.com/upload", {
+  fetch("https://mutation-mapper-2.onrender.com", {
   method: "POST",
   body: formData
 })
